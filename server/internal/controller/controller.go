2c94e39f78836159462934517cc3b66b02fad577
Naohiro Iwamoto <naohiro-iwamoto@internet.gmo>
2025-06-23 23:54:37 +0900

    add create,delete method

--- server/internal/controller/controller.go
+++ server/internal/controller/controller.go
@@ -8,6 +8,7 @@ import (
 	"inpodk8soperation/core"
 
 	"github.com/go-chi/chi/v5"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -15,6 +16,8 @@ func Controller() http.Handler {
 	r := chi.NewRouter()
 	r.Get("/", serverIndex)
 	r.Get("/{serverID}", serverDetail)
+	r.Post("/", serverCreate)
+	r.Delete("/{serverID}", serverDelete)
 	return r
 }
 
@@ -54,4 +57,56 @@ func serverDetail(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(pod)
+}
+
+func serverCreate(w http.ResponseWriter, r *http.Request) {
+	clientset, err := core.GetKubeClient()
+	if err != nil {
+		http.Error(w, "Failed to create k8s client: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var podSpec map[string]interface{}
+	if err := json.NewDecoder(r.Body).Decode(&podSpec); err != nil {
+		http.Error(w, "Invalid request body: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	// Marshal and unmarshal to concrete type
+	podBytes, err := json.Marshal(podSpec)
+	if err != nil {
+		http.Error(w, "Failed to marshal pod spec: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	var podObj = &corev1.Pod{}
+	if err := json.Unmarshal(podBytes, podObj); err != nil {
+		http.Error(w, "Failed to unmarshal pod spec: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	created, err := clientset.CoreV1().Pods(podObj.Namespace).Create(r.Context(), podObj, metav1.CreateOptions{})
+	if err != nil {
+		http.Error(w, "Failed to create pod: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(created)
+}
+
+func serverDelete(w http.ResponseWriter, r *http.Request) {
+	serverID := chi.URLParam(r, "serverID")
+	clientset, err := core.GetKubeClient()
+	if err != nil {
+		http.Error(w, "Failed to create k8s client: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var namespace, name string
+	_, err = fmt.Sscanf(serverID, "%[^/]/%s", &namespace, &name)
+	if err != nil {
+		http.Error(w, "serverID must be in namespace/name format", http.StatusBadRequest)
+		return
+	}
+	err = clientset.CoreV1().Pods(namespace).Delete(r.Context(), name, metav1.DeleteOptions{})
+	if err != nil {
+		http.Error(w, "Failed to delete pod: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
