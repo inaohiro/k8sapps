f4dbc3cefc3f5c525b0f649c5f1a6e8b840d34b3
Naohiro Iwamoto <naohiro-iwamoto@internet.gmo>
2025-06-30 17:03:55 +0900

    update

--- k6/step1.js
+++ k6/step1.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 
-const url = "http://gateway-clusterip";
+const url = "http://nginx:8080/api";
 
 export default function () {
   const namespaces = [fakeName(), fakeName(), fakeName()];
@@ -11,22 +11,15 @@ export default function () {
       headers: { "Content-Type": "application/json" },
     });
     const token = res.json().token;
+    const headers = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
 
-    http.get("http://gateway-clusterip/deployments", {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    http.get("http://gateway-clusterip/pods", {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    http.get("http://gateway-clusterip/services", {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    http.get("http://gateway-clusterip/images", {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    http.get("http://gateway-clusterip/flavors", {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    http.get(`${url}/deployments`, headers);
+    http.get(`${url}/pods`, headers);
+    http.get(`${url}/services`, headers);
+    http.get(`${url}/images`, headers);
+    http.get(`${url}/flavors`, headers);
   }
 }
 
