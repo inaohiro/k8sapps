01385da7842f06a984a0521b6b1927103bf51398
Naohiro Iwamoto <naohiro-iwamoto@internet.gmo>
2025-07-24 10:24:30 +0900

    update

--- webapp/core/middleware/intentional_error.go
+++ webapp/core/middleware/intentional_error.go
@@ -3,16 +3,24 @@ package middleware
 import (
 	"fmt"
 	"net/http"
+	"slices"
 	"time"
 )
 
-// この middleware は、middeware が実行される時間の秒数が 03 のとき
+// この middleware は、middeware が実行される時間の秒数が 00,10,20,30,40,50 のとき
 // 503 エラーを返します
 func IntentionalError(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		t := fmt.Sprintf("%d", time.Now().Unix())
 
-		if "03" == t[len(t)-2:] {
+		if slices.Contains([]string{
+			"00",
+			"10",
+			"20",
+			"30",
+			"40",
+			"50",
+		}, t[len(t)-2:]) {
 			w.WriteHeader(http.StatusServiceUnavailable)
 			w.Write([]byte(`{"error": "service temporary unavailable. please wait a second}`))
 
