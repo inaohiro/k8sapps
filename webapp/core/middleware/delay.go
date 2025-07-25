b875e5a94a9a5e8bce78e4e18a059ec2c2ef3a86
Naohiro Iwamoto <naohiro-iwamoto@internet.gmo>
2025-07-25 18:34:49 +0900

    update

--- webapp/core/middleware/delay.go
+++ webapp/core/middleware/delay.go
@@ -1,25 +1,26 @@
 package middleware
 
 import (
-	"fmt"
 	"math/rand/v2"
 	"net/http"
 	"time"
 )
 
-// この middleware は、middeware が実行される時間の秒数が 5 で終わるとき
-// 100ms ~ 999ms の遅延はさみます
-// また、50% の確率で 100ms = 999ms の遅延をはさみます
+// この middleware では以下の場合で遅延をはさみます
+// - 毎 5 分ごと, 1 分間
+// - 10% の確率
 func Delay(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		t := fmt.Sprintf("%d", time.Now().Unix())
+		_, minute, _ := time.Now().Clock()
 
-		if "5" == t[len(t)-1:] {
-			time.Sleep(time.Duration(rand.IntN(999-100)+100) * time.Millisecond)
+		// 毎 5 分ごとに, 1 分間遅延が発生する
+		if minute%5 == 0 {
+			time.Sleep(time.Duration(rand.IntN(999-300)+300) * time.Millisecond)
 		}
 
-		if rand.IntN(100) > (100 - 50) {
-			time.Sleep(time.Duration(rand.IntN(999-100)+100) * time.Millisecond)
+		// 10% の確立でさらに遅延がはいる
+		if rand.IntN(100) > (100 - 10) {
+			time.Sleep(time.Duration(rand.IntN(999-500)+500) * time.Millisecond)
 		}
 
 		next.ServeHTTP(w, r)
